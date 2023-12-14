8dacdfddf9e7296797047d813592af1130c5a0e1
John Paul Pageler Jr <harakido1@yahoo.com>
2023-12-14 18:06:51 -0500

    Created server.js and Express api

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const colors = require("@colors/colors");
+
+const app = express();
+dotenv.config();
+colors.enable();
+
+app.get("/", (req, res) => {
+    res.send("Note Categorized Library API is running...");
+});
+
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+    const note = notes.find((note) => note._id === req.params.id);
+    console.log(req.params); // Id logged to Terminal.
+
+    res.send(note); // Note rendered to PORT 5000.
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, console.log(`Server has started on PORT ${PORT}.`.blue));
