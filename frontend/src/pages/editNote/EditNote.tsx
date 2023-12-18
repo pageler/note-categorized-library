1a57a0c72404d322fb227bd0bd1318b0b0468d7f
John Paul Pageler Jr <harakido1@yahoo.com>
2023-12-18 09:58:51 -0500

    Edit Note page

--- frontend/src/pages/editNote/EditNote.tsx
+++ frontend/src/pages/editNote/EditNote.tsx
@@ -1,9 +1,42 @@
-type EditNoteProps = {};
+import { Container } from "react-bootstrap";
+import { Header } from "../../components/header/Header";
+import "./EditNote.css";
+import { NoteForm } from "../../components/NoteForm";
+import { NoteData, Tag } from "../../App";
+import { useNote } from "../../components/NoteLayout";
 
-export const EditNote = (props: EditNoteProps) => {
+type EditNoteProps = {
+    onSubmit: (id: string, data: NoteData) => void;
+    onAddTag: (tag: Tag) => void;
+    availableTags: Tag[];
+};
+
+export const EditNote = ({
+    onSubmit,
+    onAddTag,
+    availableTags,
+}: EditNoteProps) => {
+    const note = useNote();
+
     return (
         <>
-            <h1>EditNote</h1>
+            <Header />
+
+            <Container className="my-4">
+                <h1 className="editTitle">
+                    <u>Edit Note</u>
+                </h1>
+
+                <NoteForm
+                    onSubmit={(data) => onSubmit(note.id, data)}
+                    onAddTag={onAddTag}
+                    availableTags={availableTags}
+                    // populate defaultValue NoteForm:
+                    title={note.title}
+                    tags={note.tags}
+                    markdown={note.markdown}
+                />
+            </Container>
         </>
     );
 };
